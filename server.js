059075ccb1da96d8e99cd8c0d52199f189751d6c
Dev-LucasC <111757758+Dev-LucasC@users.noreply.github.com>
2022-10-27 21:43:02 -0300

    Api Livraria
    API EXPRESS NODEJS / CRUD

--- server.js
+++ server.js
@@ -1,18 +1,8 @@
-const http = require("http");
-const port = 5000;
+import app from './src/app.js'
 
-const rotas = {
-    '/': 'Curso de Node',
-    '/livros': 'Entrei na página de livros',
-    '/autores': 'Listagem de autores'
-}
+const port = process.env.PORT || 3000;
 
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(rotas[req.url]);
+app.listen(port, () => {
+  console.log(`Servidor escutando em http://localhost:${port}`)
 })
-
-server.listen(port, () => {
-    console.log(`Servidor escutando em http://localhost:${port}`);
-  })
